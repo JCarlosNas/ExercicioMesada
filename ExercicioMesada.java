c3286b2073688bc1dedc13cbdb8a4e4b4d3a1068
JCarlosNas <jose.carlosnascimento@hotmail.com>
2023-03-16 13:40:21 -0300

    Exercicio para calcular o resto de uma compra com a mesada

---
+++ ExercicioMesada.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExercicioMesada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double mesada = 50.0;
+		
+		while (mesada > 0 ) {
+			Double valorDoce = valorAleatorio();
+				
+			if( valorDoce > mesada)
+					valorDoce = mesada;
+		
+			System.out.println("Doce do valor: " + valorDoce + "Adicionado no carrinho. ");
+			mesada = mesada - valorDoce;
+		}
+		
+		System.out.println("Mesada: " + mesada);
+		System.out.println("Joaozinho gastou toda a sua mesada em doces ");
+
+	}
+	
+	private static double valorAleatorio() {
+		return ThreadLocalRandom.current().nextDouble(2, 15);
+	
+	
+	}
+
+}
